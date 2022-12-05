36484477efbef1d72d5da256b9ae020687ff0ff4
igorchi <melw3@vk.com>
2022-12-05 23:56:53 +0500

    my first commit with ssh key

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+
+
+
+// console.log('arr' + ' - obj');
+// console.log(4 + +'5');
+
+let incr = 10, 
+decr = 10;
+// incr++;
+// decr--;
+
+console.log(incr);
+console.log(decr);
+
+console.log(2*5 == 10);
+
+
